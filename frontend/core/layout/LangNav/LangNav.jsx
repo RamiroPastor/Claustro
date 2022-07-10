c89bdbce2ebab6b4eb820c38d3e52339073d3bc6
Ramiro <rammir659@gmail.com>
2022-07-11 01:23:05 +0200

    fixed url issue with LangNav

--- frontend/core/layout/LangNav/LangNav.jsx
+++ frontend/core/layout/LangNav/LangNav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import { flagES } from "frontend/assets/svg/flagES";
 import { flagGB } from "frontend/assets/svg/flagGB";
@@ -8,15 +9,17 @@ import { flagGB } from "frontend/assets/svg/flagGB";
 
 export function LangNav(props){
 
+  const current = useRouter().pathname;
+
   return(
     <div className="LangNav">
       <div className="LangNav__inner">
-        <Link href="/" locale="es">
+        <Link href={current} locale="es">
           <a>
             { flagES }
           </a>
         </Link>
-        <Link href="/" locale="en">
+        <Link href={current} locale="en">
           <a>
             { flagGB }
           </a>
