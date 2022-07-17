4868d32e6c01e34a0c7f6b3d1525685ab8b9f97f
Ramiro <rammir659@gmail.com>
2022-07-18 01:08:24 +0200

    extracted the board creation formulary into the component BoardForm

---
+++ frontend/pages/Board/BoardForm/BoardForm.jsx
@@ -0,0 +1,87 @@
+import React from "react"
+
+import { config } from "centre/config/config"
+import { langToFlag } from "frontend/base/js/langToFlag" 
+import { Msg2         } from "frontend/core/components/Msg2/Msg2"
+import { SubmitButton } from "frontend/core/components/SubmitButton/SubmitButton"
+import { TextInput    } from "frontend/core/components/TextInput/TextInput"
+
+
+
+export function BoardForm(props){
+
+  const t = props.t;
+  const extraClass   = props.extraClass;
+  const handleSubmit = props.handleSubmit;
+  const register = props.register;
+  const errors   = props.errors;
+  const watch    = props.watch
+  const createOrEdit  = props.createOrEdit;
+  const disableSubmit = props.disableSubmit;
+  const responseCode  = props.responseCode;
+
+
+
+  return(
+    <form 
+      className={"BoardForm " + extraClass}
+      onSubmit={handleSubmit}
+    >
+
+      <TextInput
+        t={t}
+        inputType="text"
+        identifier="title"
+        labelText={t("title")}
+        register={register}
+        errors={errors}
+        watch={watch}
+        isRequired={true}
+        minLen={config.board.minLen_title}
+        maxLen={config.board.maxLen_title}
+        onlyAlphanum={false}
+      />
+
+      <TextInput
+        t={t}
+        inputType="text"
+        identifier="description"
+        labelText={t("description")}
+        register={register}
+        errors={errors}
+        watch={watch}
+        isRequired={true}
+        minLen={config.board.minLen_description}
+        maxLen={config.board.maxLen_description}
+        onlyAlphanum={false}
+      />
+
+      <fieldset className="BoardForm__langSelector">
+        <legend>{t("allowedLanguages")}</legend>
+        { config.board.languages.map((lang, i) =>
+          <label key={i}>
+            <input 
+              type="checkbox"
+              value={lang}
+              {...register("lang")}
+            />
+            {langToFlag(lang)}
+            <span>{t(lang)}</span>
+          </label>
+        )}
+      </fieldset>
+
+      { responseCode !== 0 && responseCode !== 200 &&
+      <Msg2
+        isError={true}
+        message={responseCode}
+      />
+      }
+
+      <SubmitButton
+        text={t(createOrEdit)}
+        disabled={disableSubmit}
+      />
+    </form>
+  )
+}
--- frontend/pages/Board/Modal_NewBoard/Modal_NewBoard.jsx
+++ frontend/pages/Board/Modal_NewBoard/Modal_NewBoard.jsx
@@ -2,14 +2,10 @@ import React, { useState, useContext } from "react"
 import { useForm } from "react-hook-form"
 import { useRouter } from "next/router"
 
-import { config } from "centre/config/config"
 import { API        } from "frontend/base/js/axios"
-import { langToFlag } from "frontend/base/js/langToFlag" 
 import { ModalWindow  } from "frontend/core/components/ModalWindow/ModalWindow"
-import { Msg2         } from "frontend/core/components/Msg2/Msg2"
-import { SubmitButton } from "frontend/core/components/SubmitButton/SubmitButton"
-import { TextInput    } from "frontend/core/components/TextInput/TextInput"
 import { AuthContext  } from "frontend/core/contexts/AuthContext"
+import { BoardForm } from "frontend/pages/Board/BoardForm/BoardForm"
 
 
 
@@ -55,63 +51,17 @@ export function Modal_NewBoard(props) {
       setActive  = {setActive}
       title      = {t("createNewBoard")}
     >
-      <form className="Modal_NewBoard" onSubmit={handleSubmit(onSubmit)}>
-
-        <TextInput
-          t={t}
-          inputType="text"
-          identifier="title"
-          labelText={t("title")}
-          register={register}
-          errors={errors}
-          watch={watch}
-          isRequired={true}
-          minLen={config.board.minLen_title}
-          maxLen={config.board.maxLen_title}
-          onlyAlphanum={false}
-        />
-
-        <TextInput
-          t={t}
-          inputType="text"
-          identifier="description"
-          labelText={t("description")}
-          register={register}
-          errors={errors}
-          watch={watch}
-          isRequired={true}
-          minLen={config.board.minLen_description}
-          maxLen={config.board.maxLen_description}
-          onlyAlphanum={false}
-        />
-
-        <fieldset className="Modal_NewBoard__langSelector">
-          <legend>{t("allowedLanguages")}</legend>
-          { config.board.languages.map((lang, i) =>
-            <label key={i}>
-              <input 
-                type="checkbox"
-                value={lang}
-                {...register("lang")}
-              />
-              {langToFlag(lang)}
-              <span>{t(lang)}</span>
-            </label>
-          )}
-        </fieldset>
-
-        { responseCode !== 0 && responseCode !== 200 &&
-        <Msg2
-          isError={true}
-          message={responseCode}
-        />
-        }
-
-        <SubmitButton
-          text={t("create")}
-          disabled={disableSubmit}
-        />
-      </form>
+      <BoardForm
+        t = {t}
+        extraClass   = "Modal_NewBoard"
+        handleSubmit = {handleSubmit(onSubmit)}
+        register = {register}
+        errors   = {errors}
+        watch    = {watch}
+        createOrEdit  = "create"
+        disableSubmit = {disableSubmit}
+        responseCode  = {responseCode}
+      />
     </ModalWindow>
   )
 }
