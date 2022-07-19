2d7a9b9c4c0d6925599e379e238c4bd6533fffae
Ramiro <rammir659@gmail.com>
2022-07-20 00:56:32 +0200

    improved thread sorting

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { boardController } from "backend/Board/controllerBoard"
 import { threadController } from "backend/Thread/controllerThread"
+import { sortThreads      } from "backend/Thread/sortThreads"
 import { communityController} from "backend/User/controllerCommunity"
-import { sort_newerFirstWithPrio } from 'centre/utils/sort'
 import { AuthGuard } from "frontend/pages/AuthGuard/AuthGuard"
 import { Forum     } from "frontend/pages/Forum/Forum"
 
@@ -15,7 +15,7 @@ export async function getStaticProps({locale}) {
   boardList = boardList.map(x => JSON.parse(x))
   let threadList = await threadController.listThreads([])
   threadList = threadList.map(x => JSON.parse(x))
-  sort_newerFirstWithPrio(threadList, "createdAt", "pinned")
+  sortThreads(threadList)
   let userList = await communityController.listUsers([])
   userList = userList.map(x => JSON.parse(x))
   return ({ props: {...translations, boardList, threadList, userList}})
