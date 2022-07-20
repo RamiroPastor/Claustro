2251b8393f220326102c198a3f63492607875066
Ramiro <rammir659@gmail.com>
2022-07-20 20:43:51 +0200

    PostCreate backend done

---
+++ pages/api/post/create.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { postController } from "backend/Post/controllerPost"
+import { authController } from "backend/User/controllerAuth"
+
+
+
+export default async function handler(req : NextApiRequest, res : NextApiResponse) {
+  
+  const token    : String   = req.body.token;
+  const threadId : String   = req.body.threadId;
+  const body     : String   = req.body.post;
+
+  await authController.verifyUser(token).then(
+    ({code, user}) => {
+      postController.registerPost(
+        { userId: user._id
+        , threadId: threadId
+        , body: body
+        }
+      )
+    },
+    (err) => {
+      console.error(err);
+      res.status(err).json({ auth: false })
+    }
+  )
+}
