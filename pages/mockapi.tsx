0450183ed7b9a34b03bced8dbd29cd08106c8f3e
Ramiro <rammir659@gmail.com>
2022-07-24 00:40:52 +0200

    jujuju

--- pages/mockapi.tsx
+++ pages/mockapi.tsx
@@ -69,7 +69,9 @@ export async function getServerSideProps(
 
   const rawData = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-  const data : PostResData_Typicode[] = await rawData.json();
+  const dataJEJEJE : PostResData_Typicode[] = await rawData.json();
+
+  const data = dataJEJEJE.sort((a,b) => Math.random() * 10 - 5);
 
   const board : BoardResData =
     { _id: "Typicode_jsonplaceholder"
